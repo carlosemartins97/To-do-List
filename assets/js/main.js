e7cc448a024c35ab2fe467202168503ba45376fa
carlosemartins97 <carlosemartins97@gmail.com>
2020-07-11 02:46:36 -0300

    Finalizando a estilização da lista

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,6 +17,12 @@ $("input[type='text']").keypress(function(event){
     if(event.which === 13){
         var tarefaText = $(this).val();
         $(this).val("");
-        $("ul").append("<li><span class='delete'>X</span> " + tarefaText+ "</li>");
+        $("ul").append("<li><span class='delete'><i class='far fa-trash-alt'></i></span> " + tarefaText+ "</li>");
     }
 });
+
+//Fadein e fadeOut input
+$(".fa-plus").click(function(){
+    $("input[type='text']").fadeToggle();
+
+})
