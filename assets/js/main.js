d40dd967caca5f20f2fc3309914efda2c1e10b4d
carlosemartins97 <carlosemartins97@gmail.com>
2020-07-11 02:03:12 -0300

    Adicionado a lógica para adicionar novas tarefas à lista.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,22 @@
 
-$("li").click(function(){
+//Completando Tarefas
+$("ul").on("click","li", function(){
     $(this).toggleClass("completed")
 })
 
-$(".delete").on("click", function(event){
+//Deletando Tarefas
+$("ul").on("click",".delete", function(event){
     $(this).parent().fadeOut(400, function(){
         $(this).remove();
     });
     event.stopPropagation();
 })
+
+//Criando Tarefas
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        var tarefaText = $(this).val();
+        $(this).val("");
+        $("ul").append("<li><span class='delete'>X</span> " + tarefaText+ "</li>");
+    }
+});
